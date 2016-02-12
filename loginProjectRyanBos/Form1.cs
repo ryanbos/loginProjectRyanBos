6d062f3c591080e1e1a48440dd4f3ae493c77290
ryanbos <ryanbos@ed.amdsb.ca>
2016-02-12 09:30:40 -0500

    made logged in screen

---
+++ loginProjectRyanBos/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace loginProjectRyanBos
+{
+    public partial class Form1 : Form
+    {
+        public static string username = "djkhaled";
+        public static string password = "lions";
+        int test = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            if(userTextBox.Text == "djkhaled" && passTextBox.Text == "lions" )
+            {
+                
+
+                homePage hp = new homePage();
+                hp.Location = new Point((Width - hp.Width), (this.Height = hp.Height));
+                this.Controls.Add(hp);
+            }
+        }
+    }
+}
---
+++ loginProjectRyanBos/homePage.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace loginProjectRyanBos
+{
+    partial class homePage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.welcomeBackLabel = new System.Windows.Forms.Label();
+            this.logoutButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // welcomeBackLabel
+            // 
+            this.welcomeBackLabel.AutoSize = true;
+            this.welcomeBackLabel.Location = new System.Drawing.Point(131, 85);
+            this.welcomeBackLabel.Name = "welcomeBackLabel";
+            this.welcomeBackLabel.Size = new System.Drawing.Size(35, 13);
+            this.welcomeBackLabel.TabIndex = 0;
+            this.welcomeBackLabel.Text = "label1";
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.Location = new System.Drawing.Point(113, 199);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(75, 23);
+            this.logoutButton.TabIndex = 1;
+            this.logoutButton.Text = "LOG OUT";
+            this.logoutButton.UseVisualStyleBackColor = true;
+            // 
+            // homePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.Controls.Add(this.logoutButton);
+            this.Controls.Add(this.welcomeBackLabel);
+            this.Name = "homePage";
+            this.Size = new System.Drawing.Size(300, 300);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label welcomeBackLabel;
+        private System.Windows.Forms.Button logoutButton;
+    }
+}
